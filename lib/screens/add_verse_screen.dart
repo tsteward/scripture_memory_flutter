ad800b7f89db5f8d39f25299c7d1aeb8816e9278
Timothy Steward <tsteward0@gmail.com>
2018-09-09 20:54:34 -0500

    enable add button with valid reference

--- lib/screens/add_verse_screen.dart
+++ lib/screens/add_verse_screen.dart
@@ -8,16 +8,49 @@ class AddVerseScreen extends StatefulWidget {
 }
 
 class _AddVerseScreenState extends State<AddVerseScreen> {
-  final verseReferenceController = TextEditingController();
+  final referenceTextEditController = TextEditingController();
+
+  // true when the verse reference in the textbox is valid
+  bool _validReference = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // listen for edits to reference textbox
+    referenceTextEditController.addListener(_onReferenceTextEdit);
+  }
 
   @override
   void dispose() {
-    verseReferenceController.dispose();
+    referenceTextEditController.dispose();
     super.dispose();
+  }
+
+  void _onReferenceTextEdit() {
+    // when the reference textbox is edited, check that the reference is valid
+    setState(() {
+      _validReference = Verse.checkString(referenceTextEditController.text);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    // only enable add button when the reference is valid
+    void Function() addVerseOnPressed;
+    if (_validReference) {
+      addVerseOnPressed = () {
+        // only return the reference if the reference is valid
+        if (Verse.checkString(referenceTextEditController.text)) {
+          Navigator.pop<Verse>(
+              context, Verse.fromString(referenceTextEditController.text));
+        }
+      };
+    } else {
+      // null onPressed will disable the button
+      addVerseOnPressed = null;
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Add Verse'),
@@ -27,16 +60,10 @@ class _AddVerseScreenState extends State<AddVerseScreen> {
         child: Column(
           children: [
             TextField(
-              controller: verseReferenceController,
+              controller: referenceTextEditController,
             ),
             RaisedButton(
-              onPressed: () {
-                // only return the reference if the reference is valid
-                if (Verse.checkString(verseReferenceController.text)) {
-                  Navigator.pop<Verse>(
-                      context, Verse.fromString(verseReferenceController.text));
-                }
-              },
+              onPressed: addVerseOnPressed,
               child: Text('Add'),
             ),
           ],
